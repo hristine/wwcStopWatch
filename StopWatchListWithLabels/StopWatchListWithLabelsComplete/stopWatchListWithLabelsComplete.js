fc7e3261b9070fdda7a3f9319d7e0ef7ffc3b3e1
Abdella <abdellaalioncan@gmail.com>
2015-08-31 15:44:52 -0400

    added list with label

---
+++ StopWatchListWithLabels/StopWatchListWithLabelsComplete/stopWatchListWithLabelsComplete.js
@@ -0,0 +1,87 @@
+'use strict';
+
+angular.module('stopWatchListLabelsComplete', [])
+
+.controller('stopWatchListLabelsController', function($interval){
+
+    var vm = this;
+    var newTime = new Date();
+
+    this.stopWatchList = [
+      new StopWatch(newTime),
+      new StopWatch(newTime),
+      new StopWatch(newTime)
+    ];
+
+    vm.createNewStopWatch = function(listOfStopWatches, label){
+      listOfStopWatches.push(new StopWatch(newTime, label));
+      vm.newLabel = null;
+    };
+
+
+
+    function StopWatch(time, label) {
+
+      var totalElapsedMs = 0;
+      var elapsedMs = 0;
+      var startTime;
+      var timerPromise;
+
+      var self = this;
+
+      self.label = label;
+      self.start = function () {
+        if (!timerPromise) {
+          startTime = new Date();
+          timerPromise = $interval(function () {
+            var now = new Date();
+            elapsedMs = now.getTime() - startTime.getTime();
+          }, 31);
+        }
+      };
+
+      self.stop = function () {
+        if (timerPromise) {
+          $interval.cancel(timerPromise);
+          timerPromise = undefined;
+          totalElapsedMs += elapsedMs;
+          elapsedMs = 0;
+        }
+      };
+
+      self.reset = function () {
+        startTime = new Date();
+        totalElapsedMs = elapsedMs = 0;
+      };
+
+      self.getTime = function () {
+        return time;
+      };
+
+      self.getElapsedMs = function () {
+        return formatTime(totalElapsedMs + elapsedMs);
+      };
+
+      function formatTime(unformattedTime) {
+        var h, m, s, ms = 0;
+        var newTime = '';
+
+        h = Math.floor(unformattedTime / (60 * 60 * 1000));
+        unformattedTime = unformattedTime % (60 * 60 * 1000);
+        m = Math.floor(unformattedTime / (60 * 1000));
+        unformattedTime = unformattedTime % (60 * 1000);
+        s = Math.floor(unformattedTime / 1000);
+        ms = unformattedTime % 1000;
+
+        newTime = pad(h, 2) + ':' + pad(m, 2) + ':' + pad(s, 2) + ':' + pad(ms, 3);
+        return newTime;
+      }
+
+      function pad(num, size) {
+        var s = "0000" + num;
+        return s.substr(s.length - size);
+      }
+
+    }
+
+  });
